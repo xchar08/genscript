b46d8d2fde9cf2f2d0e48fe996b1b227b018554e
jpx32 <108827236+jpx32@users.noreply.github.com>
2023-02-27 01:33:59 -0600

    Update gentoo-desktop.sh

--- assets/gentoo-desktop.sh
+++ assets/gentoo-desktop.sh
@@ -1,18 +1,20 @@
-if [lspci | grep -i VGA | grep -c Intel -gt 0]
+#!/bin/bash
+
+if lspci | grep -i VGA | grep -c Intel > 0
 then
-    emerge --ask sys-firmware/intel-microcode --autounmask{,-write,-continue}
-    emerge --ask x11-drivers/xf86-video-intel --autounmask{,-write,-continue}
+emerge --ask sys-firmware/intel-microcode --autounmask{,-write,-continue}
+emerge --ask x11-drivers/xf86-video-intel --autounmask{,-write,-continue}
 fi
 
-if [lspci -nnkv | sed -n '/Network/,/^$/p' | grep -c iwlwifi -gt 0]
+if lspci -nnkv | sed -n '/Network/,/^$/p' | grep -c iwlwifi > 0
 then
-    modprobe iwlwifi
+modprobe iwlwifi
 fi
 
-if [lspci | grep -c nvidia -gt 0]
+if lspci | grep -c nvidia > 0
 then
-    emerge --ask  x11-drivers/nvidia-drivers --autounmask{,-write,-continue}
-    emerge --ask sys-firmware/nvidia-firmware --autounmask{,-write,-continue}
+emerge --ask x11-drivers/nvidia-drivers --autounmask{,-write,-continue}
+emerge --ask sys-firmware/nvidia-firmware --autounmask{,-write,-continue}
 fi
 
 #utility
@@ -26,36 +28,48 @@ emerge --ask media-fonts/source-pro --autounmask{,-write,-continue}
 emerge --ask media-fonts/noto --autounmask{,-write,-continue}
 emerge --ask media-fonts/noto-emoji --autounmask{,-write,-continue}
 
-
 #terminal
-echo -n 1 "Select terminal (alacritty, kitty, xterm): "
+echo -n "Select terminal (alacritty, kitty, xterm): "
 read term
 case "$term" in
-    "alacritty") emerge --ask x11-terms/alacritty --autounmask{,-write,-continue} ;;
-    "kitty") emerge --ask x11-terms/kitty --autounmask{,-write,-continue} ;;
-    "xterm") emerge --ask x11-terms/xterm --autounmask{,-write,-continue} ;;
+"alacritty") emerge --ask x11-terms/alacritty --autounmask{,-write,-continue} ;;
+"kitty") emerge --ask x11-terms/kitty --autounmask{,-write,-continue} ;;
+"xterm") emerge --ask x11-terms/xterm --autounmask{,-write,-continue} ;;
 esac
 
 #select desktop env
-echo -n 1 "Select desktop env (plasma, gnome): "
+echo -n "Select desktop env (plasma, gnome): "
 read denv
 case "$denv" in
-    #desktop env
-    "plasma")emerge --ask kde-plasma/plasma-meta --autounmask{,-write,-continue} && emerge --ask kde-apps/kwalletmanager --autounmask{,-write,-continue} && emerge --ask kde-plasma/kwallet-pam --autounmask{,-write,-continue}
-    && echo "#!/bin/sh\nexec dbus-launch --exit-with-session startplasma-x11">> ~/.xinitrc ;;
-    "gnome") emerge --ask gnome-base/gnome --autounmask{,-write,-continue} && emerge --ask --noreplace gui-libs/display-manager-init --autounmask{,-write,-continue} 
-    && echo "DISPLAYMANAGER=\"gdm\"" >> /etc/conf.d/display-manager && echo "exec gnome-session" > ~/.xinitrc && sed -i '1i\export XDG_MENU_PREFIX=gnome-' ~/.xinitrc ;;
+#desktop env
+"plasma") emerge --ask kde-plasma/plasma-meta --autounmask{,-write,-continue} && emerge --ask kde-apps/kwalletmanager --autounmask{,-write,-continue} && emerge --ask kde-plasma/kwallet-pam --autounmask{,-write,-continue}
+&& echo "#!/bin/sh\nexec dbus-launch --exit-with-session startplasma-x11">> ~/.xinitrc ;;
+"gnome") emerge --ask gnome-base/gnome --autounmask{,-write,-continue} && emerge --ask --noreplace gui-libs/display-manager-init --autounmask{,-write,-continue}
+&& echo "DISPLAYMANAGER="gdm"" >> /etc/conf.d/display-manager && echo "exec gnome-session" > ~/.xinitrc && sed -i '1i\export XDG_MENU_PREFIX=gnome-' ~/.xinitrc ;;
 esac
 
-echo -n 1 "Get widgets? [y/n]" 
+echo -n "Get widgets? [y/n]: "
 read widg
-if [[ "$widg" == 'y' ]]; 
-    if [["$denv" == "plasma"]];
-        then emerge --ask kde-plasma/kdeplasma-addons; 
-    else 
-        emerge --ask gnome-extra/gnome-shell-extensions
-    fi
+if [[ "$widg" == 'y' ]]; then
+if [[ "$denv" == "plasma" ]]; then
+emerge --ask kde-plasma/kdeplasma-addons
+else
+emerge --ask gnome-extra/gnome-shell-extensions
+fi
 fi
 
-rc-update add display-manager default 
-rc-service display-manager start 
+#set up zram
+modprobe zram
+echo $((6144*1024*1024)) > /sys/block/zram0/disksize
+mkswap /dev/zram0
+swapon /dev/zram0 -p 10
+touch /etc/local.d/zram.start
+touch /etc/local.d/zram.stop
+chmod +x /etc/local.d/zram.start
+chmod +x /etc/local.d/zram.stop
+rc-update add local default
+
+modprobe nvidia
+
+rc-update add display-manager default
+rc-service display-manager start
