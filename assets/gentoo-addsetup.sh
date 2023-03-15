d04dab1da53f36dc703d5da33ca8c8ef17f8daaf
jpx32 <jp2888sx@gmail.com>
2023-03-15 14:43:25 -0500

    sourced fish

--- assets/gentoo-addsetup.sh
+++ assets/gentoo-addsetup.sh
@@ -8,6 +8,7 @@ sudo emerge app-shells/fish --autounmask{,-write,-continue}
 
 curl -o ~/.bashrc https://raw.githubusercontent.com/jpx32/dotfiles-space/master/~/.bashrc
 source ~/.bashrc
+source ~/.config/fish/config.fish
 
 # add eselect repositories
 
