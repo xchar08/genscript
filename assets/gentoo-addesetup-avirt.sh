71318dbaa31c02a2cccbb710815017c1fc035cb7
jpx32 <jp2888sx@gmail.com>
2023-03-18 14:42:43 -0500

    added auditd and sysklogd

--- assets/gentoo-addesetup-avirt.sh
+++ assets/gentoo-addesetup-avirt.sh
@@ -88,5 +88,35 @@ sudo ufw enable
 sudo emerge net-analyzer/fail2ban --autounmask{,-write,-continue}
 sudo emerge dev-python/pyinotify --autounmask{,-write,-continue}
 
+sudo sed -i 's/enabled = false/enabled = true/g' /etc/fail2ban/jail.conf
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+sudo mv filter.d/ jail.d/
+
 sudo rc-update add fail2ban default
 sudo rc-service fail2ban start
+
+sudo emerge sys-process/audit --autounmask{,-write,-continue}
+sudo rc-update add auditd default
+sudo rc-service auditd start
+
+
+#thunderbird
+
+sudo emerge mail-client/thunderbird --autounmask{,-write,-continue}
+xdg-mime default thunderbird.desktop x-scheme-handler/mailto
+
+#klogd
+
+sudo emerge app-admin/sysklogd --autounmask{,-write,-continue}
+sudo dispatch-conf
+sudo emerge app-admin/sysklogd --autounmask{,-write,-continue}
+sudo rc-update add sysklogd default
+
+sudo wget https://raw.githubusercontent.com/jpx32/dotfiles-space/main/etc/syslog.conf -O /etc/syslog.conf
+sudo bash -c 'echo -e "# Config file for /etc/init.d/sysklogd\n\nSYSLOGD=\"-m 0 -s -s -k\"" > /etc/conf.d/sysklogd'
+
+sudo rc-service sysklogd start
+
+#add a password to bash 
+
+#sudo bash -c 'read -p "Enter a superuser username: " username && echo "set superusers=\"$username\"" >> /etc/grub.d/40_custom; read -s -p "Enter a superuser password: " password && echo -e "\npassword_pbkdf2 $username $(grub-mkpasswd-pbkdf2 <<< "$password")" >> /etc/grub.d/40_custom && grub-mkconfig -o /boot/grub/grub.cfg'
