61b3039958e71421944bdb1e97b81349f24063b4
jpx32 <jp2888sx@gmail.com>
2023-03-30 12:06:26 -0500

    fixed ntpd?

--- assets/gentoo-addesetup-avirt.sh
+++ assets/gentoo-addesetup-avirt.sh
@@ -189,3 +189,8 @@ sudo mktwpol.sh -u
 sudo twadmin --create-polfile /etc/tripwire/twpol.txt
 sudo tripwire --init
 sudo tripwire --check
+
+#fix ntp
+sudo rc-service ntpd start && sleep 1 && sudo ntpq -p | awk 'NR>2 && $3=="-" {print $1}' | xargs -I{} sudo ntpq -r remove {} && sudo ntpdate ntp.pool.org && echo "server ntp.pool.org" | sudo tee -a /etc/ntp.conf > /dev/null && sudo rc-service ntpd restart
+
+#restart system
