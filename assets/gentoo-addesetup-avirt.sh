b4ffa16775c2994512bdca19c799a12472a773a9
jpx32 <jp2888sx@gmail.com>
2023-03-31 08:20:14 -0500

    added binfmt-misc module loading

--- assets/gentoo-addesetup-avirt.sh
+++ assets/gentoo-addesetup-avirt.sh
@@ -190,6 +190,9 @@ sudo twadmin --create-polfile /etc/tripwire/twpol.txt
 sudo tripwire --init
 sudo tripwire --check
 
+#modprobe binfmt-misc
+sudo modprobe binfmt-misc
+
 #fix ntp
 sudo rc-service ntpd start && sleep 1 && sudo ntpq -p | awk 'NR>2 && $3=="-" {print $1}' | xargs -I{} sudo ntpq -r remove {} && sudo ntpdate ntp.pool.org && echo "server ntp.pool.org" | sudo tee -a /etc/ntp.conf > /dev/null && sudo rc-service ntpd restart
 
