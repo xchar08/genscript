2d677fbd094599a2ce2d76c296dc596e95e79d9c
jpx32 <108827236+jpx32@users.noreply.github.com>
2023-05-20 20:27:41 +0000

    Update gentoo-addesetup-avirt.sh

--- assets/gentoo-addesetup-avirt.sh
+++ assets/gentoo-addesetup-avirt.sh
@@ -171,6 +171,8 @@ sudo modprobe binfmt-misc
 sudo chmod -R go-rwx ~
 echo 'to change them back, run sudo chown $username /path/'
 
+sudo emerge ntp
+
 #fix ntp
 sudo rc-service ntpd start && sleep 1 && sudo ntpq -p | awk 'NR>2 && $3=="-" {print $1}' | xargs -I{} sudo ntpq -r remove {} && sudo ntpdate ntp.pool.org && echo "server ntp.pool.org" | sudo tee -a /etc/ntp.conf > /dev/null && sudo rc-service ntpd restart
 
