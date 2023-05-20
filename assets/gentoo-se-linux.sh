9f36d40877af2baae0a2232e45162026103d69e8
jpx32 <108827236+jpx32@users.noreply.github.com>
2023-05-20 22:11:48 +0000

    Update gentoo-se-linux.sh

--- assets/gentoo-se-linux.sh
+++ assets/gentoo-se-linux.sh
@@ -4,12 +4,27 @@ echo "Do you want to enable SELinux? [y/N]"
 read answer
 if [[ "$answer" != "${answer#[Yy]}" ]]; then
     sudo emerge selinux-base-policy selinux-policykit sec-policy/selinux-mozilla policycoreutils
-    echo 'rc_security="YES"' | sudo tee -a /etc/rc.conf
-    sudo setenforce 1
-    sudo semanage user -a -R "staff_r sysadm_r user_r" -L s0 -M targeted -s user_u $USER
-    sudo audit2allow -M ${USER}_firefox < /var/log/audit/audit.log
-    sudo semodule -i ${USER}_firefox.pp
-    sudo restorecon -R -v /home/$USER
+
+    # Check if the audit log file exists and has the correct permissions
+    audit_log="/var/log/audit/audit.log"
+    if [[ -f "$audit_log" && -r "$audit_log" ]]; then
+        sudo semodule -DB # Disable SELinux policies
+        sudo setenforce 0 # Set SELinux to permissive mode temporarily
+
+        # Generate SELinux policy modules from audit log
+        sudo ausearch -c 'firefox' --raw | sudo audit2allow -M ${USER}_firefox
+
+        # Load the generated policy module
+        sudo semodule -i ${USER}_firefox.pp
+
+        # Re-enable SELinux and restore the correct permissions
+        sudo setenforce 1
+        sudo semodule -B # Re-enable SELinux policies
+        sudo restorecon -R -v /home/$USER
+    else
+        echo "Audit log file not found or inaccessible."
+        echo "Please ensure that SELinux is properly configured and the audit log is available."
+    fi
 else
     echo "SELinux is not enabled."
 fi
