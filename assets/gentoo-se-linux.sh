8210d83ffd39881dd5347faddf843b465f87a39c
jpx32 <108827236+jpx32@users.noreply.github.com>
2023-05-20 20:45:07 +0000

    Update gentoo-se-linux.sh

--- assets/gentoo-se-linux.sh
+++ assets/gentoo-se-linux.sh
@@ -3,17 +3,15 @@
 echo "Do you want to enable SELinux? [y/N]"
 read answer
 if [[ "$answer" != "${answer#[Yy]}" ]]; then
-    sudo emerge selinux-base-policy selinux-policykit sec-policy/selinux-mozilla
+    sudo emerge selinux-base-policy selinux-policykit sec-policy/selinux-mozilla audit2allow
     echo 'rc_security="YES"' | sudo tee -a /etc/rc.conf
     sudo setenforce 1
-    sudo genpol -v -r /etc -o /etc/selinux/strict/policy.kern
     sudo semanage user -a -R "staff_r sysadm_r user_r" -L s0 -M targeted -s user_u $USER
     sudo auditctl -w /usr/bin/firefox -p x -k firefox
     sudo auditctl -w /usr/bin/firefox -p x -k firefox
     sudo audit2allow -M ${USER}_firefox < /var/log/audit/audit.log
     sudo semodule -i ${USER}_firefox.pp
-    sudo chcon -R -t user_home_t /home/$USER
-    sudo genpol -v -r /etc -o /etc/selinux/strict/policy.kern
+    sudo restorecon -R -v /home/$USER
 else
     echo "SELinux is not enabled."
 fi
