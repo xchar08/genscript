e32105de48d55909afba00e3954c5732ac0a8cfe
jpx32 <108827236+jpx32@users.noreply.github.com>
2023-05-20 22:20:24 +0000

    Update gentoo-se-linux.sh

--- assets/gentoo-se-linux.sh
+++ assets/gentoo-se-linux.sh
@@ -16,31 +16,3 @@ else
     sudo reboot
 fi
 
-# Wait for the system to reboot and run the rest of the configuration
-sleep 60
-
-# Set SELinux to enforcing mode
-sudo setenforce 1
-
-# Check if the audit log directory exists and has the correct permissions
-audit_log_dir="/var/log/audit"
-if [[ -d "$audit_log_dir" && -r "$audit_log_dir" ]]; then
-    echo "Audit log directory exists and is accessible."
-else
-    echo "Audit log directory not found or inaccessible. Creating..."
-    sudo mkdir -p "$audit_log_dir"
-    sudo chmod 750 "$audit_log_dir"
-fi
-
-# Check if the audit log file exists and has the correct permissions
-audit_log="/var/log/audit/audit.log"
-if [[ -f "$audit_log" && -r "$audit_log" ]]; then
-    echo "Audit log file exists and is accessible."
-else
-    echo "Audit log file not found or inaccessible. Creating..."
-    sudo touch "$audit_log"
-    sudo chmod 640 "$audit_log"
-    sudo chown root:root "$audit_log"
-fi
-
-# Additional configuration steps can be added here
