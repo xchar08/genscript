37055f3d1b82c0be23e46c2c13a12ca3fa46b95b
jpx32 <108827236+jpx32@users.noreply.github.com>
2023-06-22 01:43:35 -0500

    fixed partitioning

--- assets/0-partitioning.sh
+++ assets/0-partitioning.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 
-# read in all volumes 
-
-echo -n 'Enter device label (e.x. nvme0n1): '
+# Read in all volumes
+echo -n 'Enter device label (e.g., nvme0n1): '
 read -r primpart
-echo -n 'Enter efi partition (e.x. nvme0n1p1): '
+echo -n 'Enter efi partition (e.g., nvme0n1p1): '
 read -r efipart
-echo -n 'Enter boot partition (e.x. nvme0n1p2): '
+echo -n 'Enter boot partition (e.g., nvme0n1p2): '
 read -r bootpart
-echo -n 'Enter root partition (e.x. nvme0n1p3): '
+echo -n 'Enter root partition (e.g., nvme0n1p3): '
 read -r rootpart
 
-# Create partitions taking up 40% of the total device drive
-parted -a optimal /dev/"$primpart" -- mklabel gpt
-parted -a optimal /dev/"$primpart" -- mkpart ESP fat32 0% 512MiB
-parted -a optimal /dev/"$primpart" -- mkpart swap linux-swap 512MiB 8.75GiB
-parted -a optimal /dev/"$primpart" -- mkpart rootfs btrfs 8.75GiB 26%
-parted -a optimal /dev/"$primpart" -- mkpart var btrfs 26% 34%
-parted -a optimal /dev/"$primpart" -- mkpart tmp btrfs 34% 40%
-parted -a optimal /dev/"$primpart" -- set 1 boot on
+# Prompt user for total size
+echo -n 'Enter total size for partitions (e.g., 100G): '
+read -r totalsize
+
+# Calculate partition sizes
+efi_size="512MiB"
+boot_size="1GiB"
+swap_size="8.75GiB"
+var_size=$(echo "0.13 * $totalsize" | bc)   # 13% of the total size
+tmp_size=$(echo "0.06 * $totalsize" | bc)   # 6% of the total size
+root_size=$(echo "$totalsize - $efi_size - $boot_size - $swap_size - $var_size - $tmp_size" | bc)
+
+# Create partitions based on calculated sizes
+parted -a optimal "/dev/$primpart" -- mklabel gpt
+parted -a optimal "/dev/$primpart" -- mkpart ESP fat32 0% "$efi_size"
+parted -a optimal "/dev/$primpart" -- mkpart boot ext4 "$efi_size" "$boot_size"
+parted -a optimal "/dev/$primpart" -- mkpart swap linux-swap "$boot_size" +"$swap_size"
+parted -a optimal "/dev/$primpart" -- mkpart rootfs btrfs +"$swap_size" +"$root_size"
+parted -a optimal "/dev/$primpart" -- mkpart var btrfs +"$root_size" +"$var_size"
+parted -a optimal "/dev/$primpart" -- mkpart tmp btrfs +"$var_size" +"$tmp_size"
+parted -a optimal "/dev/$primpart" -- set 1 boot on
 
 # Format partitions
-mkfs.fat -F32 /dev/"$efipart"
-mkfs.ext4 /dev/"$bootpart"
+mkfs.fat -F32 "/dev/$efipart"
+mkfs.ext4 "/dev/$bootpart"
 
 # Encrypt root partition
-cryptsetup luksFormat --type luks2 /dev/"$rootpart"
-cryptsetup luksDump /dev/"$rootpart"
+cryptsetup luksFormat --type luks2 "/dev/$rootpart"
+cryptsetup luksDump "/dev/$rootpart"
 blkid
 
 partuuid=$(blkid | grep "crypto_LUKS" | sed -n 's/.*PARTUUID=\"\([^\"]*\)\".*/\1/p')
 
-cryptsetup open /dev/"$rootpart" luks-"$partuuid"
+cryptsetup open "/dev/$rootpart" "luks-$partuuid"
