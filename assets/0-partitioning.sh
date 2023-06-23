9160bab446493a9cd767393213b9064614574cf5
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-06-23 06:02:24 +0000

    Update 0-partitioning.sh

--- assets/0-partitioning.sh
+++ assets/0-partitioning.sh
@@ -6,25 +6,28 @@ read -p 'Enter efi partition (e.g., nvme0n1p1): ' efipart
 read -p 'Enter boot partition (e.g., nvme0n1p2): ' bootpart
 read -p 'Enter root partition (e.g., nvme0n1p3): ' rootpart
 
-# Prompt user for total size
-read -p 'Enter total size for partitions (e.g., 100G): ' totalsize
+# Prompt user for total size in GB
+read -p 'Enter total size for partitions (e.g., 100G): ' totalsize_gb
+
+# Convert total size to MiB
+totalsize_mib=$(echo "$totalsize_gb * 953.674" | bc)
 
 # Calculate partition sizes
 efi_size="512MiB"
 boot_size="1GiB"
 swap_size="8.75GiB"
-var_size=$(echo "0.13 * $totalsize" | bc)   # 13% of the total size
-tmp_size=$(echo "0.06 * $totalsize" | bc)   # 6% of the total size
-root_size=$(echo "$totalsize - ${efi_size%MiB} - ${boot_size%GiB} - ${swap_size%GiB} - $var_size - $tmp_size" | bc)
+var_size=$(echo "0.13 * $totalsize_mib" | bc)   # 13% of the total size
+tmp_size=$(echo "0.06 * $totalsize_mib" | bc)   # 6% of the total size
+root_size=$(echo "$totalsize_mib - ${efi_size%MiB} - (${boot_size%GiB} * 1024) - (${swap_size%GiB} * 1024) - $var_size - $tmp_size" | bc)
 
 # Create partitions based on calculated sizes
 parted -a optimal "/dev/$primpart" mklabel gpt
-parted -a optimal "/dev/$primpart" mkpart ESP fat32 0% "${efi_size}"
-parted -a optimal "/dev/$primpart" mkpart boot ext4 "${efi_size}" "${boot_size}"
-parted -a optimal "/dev/$primpart" mkpart swap linux-swap "${boot_size}" +"${swap_size}"
-parted -a optimal "/dev/$primpart" mkpart rootfs btrfs +"${swap_size}" +"${root_size}"
-parted -a optimal "/dev/$primpart" mkpart var btrfs +"${root_size}" +"${var_size}"
-parted -a optimal "/dev/$primpart" mkpart tmp btrfs +"${var_size}" +"${tmp_size}"
+parted -a optimal "/dev/$primpart" mkpart ESP fat32 0% "${efi_size}MiB"
+parted -a optimal "/dev/$primpart" mkpart boot ext4 "${efi_size}MiB ${boot_size}MiB"
+parted -a optimal "/dev/$primpart" mkpart swap linux-swap "${boot_size}MiB ${swap_size}MiB"
+parted -a optimal "/dev/$primpart" mkpart rootfs btrfs "${swap_size}MiB ${root_size}MiB"
+parted -a optimal "/dev/$primpart" mkpart var btrfs "${root_size}MiB ${var_size}MiB"
+parted -a optimal "/dev/$primpart" mkpart tmp btrfs "${var_size}MiB ${tmp_size}MiB"
 parted -a optimal "/dev/$primpart" set 2 boot on
 
 # Format partitions
