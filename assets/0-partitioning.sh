99ca04e60203ef9df2715e1d1c480361cb9d3cc5
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-06-23 06:07:14 +0000

    Update 0-partitioning.sh

--- assets/0-partitioning.sh
+++ assets/0-partitioning.sh
@@ -18,16 +18,30 @@ boot_size="1GiB"
 swap_size="8.75GiB"
 var_size=$(echo "0.13 * $totalsize_mib" | bc)   # 13% of the total size
 tmp_size=$(echo "0.06 * $totalsize_mib" | bc)   # 6% of the total size
-root_size=$(echo "$totalsize_mib - ${efi_size%MiB} - (${boot_size%GiB} * 1024) - (${swap_size%GiB} * 1024) - $var_size - $tmp_size" | bc)
+
+# Calculate partition start and end points
+efi_start=0
+efi_end="${efi_size}"
+boot_start="${efi_end}"
+boot_end=$(echo "${boot_start} + ${boot_size}" | bc)
+swap_start="${boot_end}"
+swap_end=$(echo "${swap_start} + ${swap_size}" | bc)
+root_start="${swap_end}"
+root_end=$(echo "${root_start} + ${root_size}" | bc)
+var_start="${root_end}"
+var_end=$(echo "${var_start} + ${var_size}" | bc)
+tmp_start="${var_end}"
+#tmp_end=$(echo "${tmp_start} + ${tmp_size}" | bc)
+tmp_end="${totalsize_mib}"
 
 # Create partitions based on calculated sizes
 parted -a optimal "/dev/$primpart" mklabel gpt
-parted -a optimal "/dev/$primpart" mkpart ESP fat32 0% "${efi_size}MiB"
-parted -a optimal "/dev/$primpart" mkpart boot ext4 "${efi_size}MiB ${boot_size}MiB"
-parted -a optimal "/dev/$primpart" mkpart swap linux-swap "${boot_size}MiB ${swap_size}MiB"
-parted -a optimal "/dev/$primpart" mkpart rootfs btrfs "${swap_size}MiB ${root_size}MiB"
-parted -a optimal "/dev/$primpart" mkpart var btrfs "${root_size}MiB ${var_size}MiB"
-parted -a optimal "/dev/$primpart" mkpart tmp btrfs "${var_size}MiB ${tmp_size}MiB"
+parted -a optimal "/dev/$primpart" mkpart ESP fat32 "${efi_start}MiB" "${efi_end}MiB"
+parted -a optimal "/dev/$primpart" mkpart boot ext4 "${boot_start}MiB" "${boot_end}MiB"
+parted -a optimal "/dev/$primpart" mkpart swap linux-swap "${swap_start}MiB" "${swap_end}MiB"
+parted -a optimal "/dev/$primpart" mkpart rootfs btrfs "${root_start}MiB" "${root_end}MiB"
+parted -a optimal "/dev/$primpart" mkpart var btrfs "${var_start}MiB" "${var_end}MiB"
+parted -a optimal "/dev/$primpart" mkpart tmp btrfs "${tmp_start}MiB" "${tmp_end}MiB"
 parted -a optimal "/dev/$primpart" set 2 boot on
 
 # Format partitions
