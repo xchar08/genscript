3cd28cd75b1cede04922eee0af1903b37bb166f5
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-06-25 20:46:42 +0000

    Update 2-tar.sh

--- assets/2-tar.sh
+++ assets/2-tar.sh
@@ -9,6 +9,6 @@ tar xvJpf stage3-*.tar.xz --xattrs --numeric-owner
 
 # prepare for chroot
 
-mkdir /mnt/gentoo/etc/portage/repos.conf
+mkdir -p /mnt/gentoo/etc/portage/repos.conf
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 cp /etc/resolv.conf /mnt/gentoo/etc/resolv.conf
