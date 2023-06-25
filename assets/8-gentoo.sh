5e114ed2e6a83a2e8ee74083d85a4d88b51a4e0b
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-06-25 22:59:02 +0000

    Update 8-gentoo.sh

--- assets/8-gentoo.sh
+++ assets/8-gentoo.sh
@@ -5,6 +5,7 @@ cat /etc/portage/package.use/sys-boot >> /etc/portage/package.use~
 cat /etc/portage/package.use/99local.conf >> /etc/portage/package.use~
 rm -rf /etc/portage/package.use/
 mv /etc/portage/package.use~ /etc/portage/package.use
+cat /root/gentoo-secured-dwl/assets/package.use >> /etc/portage/package.use
 
 passwd
 
