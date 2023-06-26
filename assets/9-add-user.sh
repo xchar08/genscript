22e673fca5afa8b0016e616e358c4cfce7a95daf
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-06-26 06:58:36 +0000

    Update 9-add-user.sh

--- assets/9-add-user.sh
+++ assets/9-add-user.sh
@@ -6,6 +6,8 @@ emerge app-admin/sudo --autounmask{,-write,-continue}
 
 echo "Please enter the username you would like to create in Gentoo:"
 read -r us
+useradd "$us"
+groupadd user
 usermod -aG wheel,video,audio,user,portage,plugdev "$us"
 echo "$us" | sudo tee /etc/hostname
 echo "%wheel ALL=(ALL:ALL) ALL" | sudo tee -a /etc/sudoers
