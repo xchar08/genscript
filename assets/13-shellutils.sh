cd4b48244e048f30ce734b5b6a8e24b84748930e
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-06-26 07:03:30 +0000

    Update 13-shellutils.sh

--- assets/13-shellutils.sh
+++ assets/13-shellutils.sh
@@ -8,10 +8,10 @@ su "$us"
 sudo emerge app-shells/fish --autounmask{,-write,-continue}
 
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | sudo sh
 sudo eval "$(starship init zsh)"
 
 # add nitch and neofetch
 
-wget https://raw.githubusercontent.com/unxsh/nitch/main/setup.sh && sh setup.sh
+wget https://raw.githubusercontent.com/unxsh/nitch/main/setup.sh && sudo sh setup.sh
 sudo emerge app-misc/neofetch --autounmask{,-write,-continue}
