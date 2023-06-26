940de1402cfddc3dcf9e58d7131b50f91a5a1abf
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-06-26 01:32:32 +0000

    Update 9-add-user.sh

--- assets/9-add-user.sh
+++ assets/9-add-user.sh
@@ -6,7 +6,7 @@ emerge app-admin/sudo --autounmask{,-write,-continue}
 
 echo "Please enter the username you would like to create in Gentoo:"
 read -r us
-usermod -aG wheel,video,audio,user,portage "$us"
+usermod -aG wheel,video,audio,user,portage,plugdev "$us"
 echo "$us" | sudo tee /etc/hostname
 echo "%wheel ALL=(ALL:ALL) ALL" | sudo tee -a /etc/sudoers
 echo -e "Defaults rootpw\nDefaults !tty_tickets" | sudo tee -a /etc/sudoers
