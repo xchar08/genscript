d460c4bd3eabaf65052a8ad366fc3a207070e68e
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-06-27 17:14:26 +0000

    Update 10-video-system.sh

--- assets/10-video-system.sh
+++ assets/10-video-system.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-if lspci | grep -i VGA | grep -c Intel > 0
-then
+if lspci | grep -i VGA | grep -q Intel; then
     sudo emerge sys-firmware/intel-microcode --autounmask{,-write,-continue}
     sudo emerge x11-drivers/xf86-video-intel --autounmask{,-write,-continue}
 fi
 
-if lspci -nnkv | sed -n '/Network/,/^$/p' | grep -c iwlwifi > 0
-then
+if lspci -nnkv | sed -n '/Network/,/^$/p' | grep -q iwlwifi; then
     sudo modprobe iwlwifi
 fi
 
-if lspci | grep -c nvidia > 0
-then
-    read -p "Nvidia or Noveau: " input
+if lspci | grep -q nvidia; then
+    read -p "Nvidia or Nouveau: " input
 
     sudo emerge sys-firmware/nvidia-firmware --autounmask{,-write,-continue}
 
     if [ "$input" = "nvidia" ]; then
         sudo emerge x11-drivers/nvidia-drivers --autounmask{,-write,-continue}
         sudo modprobe nvidia
-    if [ "$input" = "nvidia" ]; then
+    elif [ "$input" = "nouveau" ]; then
         sudo emerge x11-drivers/xf86-video-nouveau --autounmask{,-write,-continue}
         sudo modprobe nouveau
     else
@@ -28,12 +25,12 @@ then
     fi
 fi
 
-if lspci | grep -i VGA | grep -c AMD > 0; then
+if lspci | grep -i VGA | grep -q AMD; then
     sudo emerge x11-drivers/xf86-video-amdgpu --autounmask{,-write,-continue}
     modprobe amdgpu
 fi
 
-if lspci -nnkv | sed -n '/Network/,/^$/p' | grep -c "Atheros\|Qualcomm" > 0; then
+if lspci -nnkv | sed -n '/Network/,/^$/p' | grep -qE "Atheros|Qualcomm"; then
     modprobe ath9k
 fi
 
