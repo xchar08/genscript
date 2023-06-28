de8dba38e807b8ad3fa0ee54bf968a1129e0a305
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-06-28 06:56:39 +0000

    Update 12-zram.sh

--- assets/12-zram.sh
+++ assets/12-zram.sh
@@ -2,7 +2,7 @@
 
 # Function to check if a device is busy
 function is_device_busy() {
-  lsof "$1" >/dev/null 2>&1
+  sudo lsof "$1" >/dev/null 2>&1
 }
 
 # Function to unmount zram or loop device if it's in use
@@ -12,73 +12,72 @@ function unmount_device() {
   
   if [[ -n "$mount_point" ]]; then
     echo "Unmounting $device from $mount_point..."
-    umount "$device"
+    sudo umount "$device"
   fi
   
   if is_device_busy "$device"; then
     echo "Detaching $device..."
-    losetup -d "$device"
+    sudo losetup -d "$device"
   fi
 }
+
+read -p "Enter the path or device name of the swap partition: " swap_partition
 
 read -p "Do you want to set up regular zram or encrypted zram? (r/e)" choice
 
 case "$choice" in
   r|R )
     # Set up regular zram
-    modprobe zram
+    sudo modprobe zram
     
     # Unmount and detach zram0 if it's in use
     unmount_device "/dev/zram0"
-    echo $((6144*1024*1024)) > /sys/block/zram0/disksize
+    echo $((6144*1024*1024)) | sudo tee /sys/block/zram0/disksize > /dev/null
     
-    mkswap /dev/zram0
-    swapon /dev/zram0 -p 10
+    sudo mkswap /dev/zram0
+    sudo swapon /dev/zram0 -p 10
     
-    cat << EOF > /etc/local.d/zram.start
+    sudo bash -c 'cat << EOF > /etc/local.d/zram.start
 #!/bin/sh
 modprobe zram
 echo $((6144*1024*1024)) > /sys/block/zram0/disksize
 mkswap /dev/zram0
 swapon /dev/zram0 -p 10
-EOF
+EOF'
 
-    cat << EOF > /etc/local.d/zram.stop
+    sudo bash -c 'cat << EOF > /etc/local.d/zram.stop
 #!/bin/sh
 swapoff /dev/zram0
 echo 1 > /sys/block/zram0/reset
 echo 0 > /sys/block/zram0/disksize
 modprobe -r zram
-EOF
+EOF'
     
-    chmod +x /etc/local.d/zram.start
-    chmod +x /etc/local.d/zram.stop
+    sudo chmod +x /etc/local.d/zram.start
+    sudo chmod +x /etc/local.d/zram.stop
     
-    rc-update add local default
+    sudo rc-update add local default
     ;;
   e|E )
     # Generate a random passphrase for encryption
-    passphrase=$(openssl rand -hex 32)
+    passphrase=$(sudo openssl rand -hex 32)
     
     # Update /etc/crypttab
-    echo "swap      /dev/<device>    /dev/urandom   swap,cipher=aes-xts-plain64,size=8192" | sudo tee -a /etc/crypttab > /dev/null
-    
-    # Replace <device> with the actual device name or path for your swap partition
+    sudo bash -c "echo 'swap      $swap_partition    /dev/urandom   swap,cipher=aes-xts-plain64,size=8192' >> /etc/crypttab"
     
     # Update /etc/fstab
-    echo "/dev/mapper/swap  none   swap    defaults   0       0" | sudo tee -a /etc/fstab > /dev/null
+    sudo bash -c "echo '/dev/mapper/swap  none   swap    defaults   0       0' >> /etc/fstab"
     
     # Enable encrypted swap on boot
     sudo rc-update add swap boot
     
     # Set the passphrase for the swap partition
-    echo "swap_crypt UUID=$(blkid -s UUID -o value /dev/<device>) none luks,keyscript=decrypt_keyctl" | sudo tee
-/etc/conf.d/dmcrypt_swap_passphrase > /dev/null
-    echo "passphrase=$passphrase" | sudo tee -a /etc/conf.d/dmcrypt_swap_passphrase > /dev/null
-
+    sudo bash -c "echo 'swap_crypt UUID=$(sudo blkid -s UUID -o value $swap_partition) none luks,keyscript=decrypt_keyctl' > /etc/conf.d/dmcrypt_swap_passphrase"
+    sudo bash -c "echo 'passphrase=$passphrase' >> /etc/conf.d/dmcrypt_swap_passphrase"
+    
     # Set the correct permissions for the passphrase file
     sudo chmod 0600 /etc/conf.d/dmcrypt_swap_passphrase
-
+    
     # Update the initramfs
     sudo dracut --force --regenerate-all
     ;;
