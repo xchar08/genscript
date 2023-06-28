4d56e18442b3caca51cd55f448e7dd812933dbff
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-06-28 06:34:06 +0000

    Update 12-zram.sh

--- assets/12-zram.sh
+++ assets/12-zram.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-#setting up ram
-
-# Function to check if a device is busy
-function is_device_busy() {
-  lsof "$1" >/dev/null 2>&1
-}
-
-# Function to unmount zram or loop device if it's in use
-function unmount_device() {
-  local device="$1"
-  local mount_point=$(findmnt -n -o TARGET --source "$device" 2>/dev/null)
-  
-  if [[ -n "$mount_point" ]]; then
-    echo "Unmounting $device from $mount_point..."
-    umount "$device"
-  fi
-  
-  if is_device_busy "$device"; then
-    echo "Detaching $device..."
-    losetup -d "$device"
-  fi
-}
-
-#!/bin/bash
-
 # Function to check if a device is busy
 function is_device_busy() {
   lsof "$1" >/dev/null 2>&1
@@ -82,12 +57,11 @@ EOF
     rc-update add local default
     ;;
   e|E )
-   
     # Generate a random passphrase for encryption
-    passphrase=$(sudo openssl rand -hex 32)
+    passphrase=$(openssl rand -hex 32)
     
     # Update /etc/crypttab
-    echo "swap      <device>    /dev/urandom   swap,cipher=aes-xts-plain64,size=8192" | sudo tee -a /etc/crypttab > /dev/null
+    echo "swap      /dev/<device>    /dev/urandom   swap,cipher=aes-xts-plain64,size=8192" | sudo tee -a /etc/crypttab > /dev/null
     
     # Replace <device> with the actual device name or path for your swap partition
     
@@ -98,12 +72,13 @@ EOF
     sudo rc-update add swap boot
     
     # Set the passphrase for the swap partition
-    echo "swap_crypt UUID=$(sudo blkid -s UUID -o value <device>) none luks,keyscript=decrypt_keyctl" | sudo tee /etc/conf.d/dmcrypt_swap_passphrase > /dev/null
+    echo "swap_crypt UUID=$(blkid -s UUID -o value /dev/<device>) none luks,keyscript=decrypt_keyctl" | sudo tee
+/etc/conf.d/dmcrypt_swap_passphrase > /dev/null
     echo "passphrase=$passphrase" | sudo tee -a /etc/conf.d/dmcrypt_swap_passphrase > /dev/null
-    
+
     # Set the correct permissions for the passphrase file
     sudo chmod 0600 /etc/conf.d/dmcrypt_swap_passphrase
-    
+
     # Update the initramfs
     sudo dracut --force --regenerate-all
     ;;
