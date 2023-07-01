f805d63e1e7c598770dbb576b6af72a1eac2a1bb
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-07-01 21:41:25 +0000

    Update 21-programming.sh

--- assets/21-programming.sh
+++ assets/21-programming.sh
@@ -4,6 +4,7 @@ sudo emerge virtual/jdk --autounmask{,-write,-continue}
 sudo emerge virtual/jre --autounmask{,-write,-continue}
 sudo emerge dev-java/ant-core --autounmask{,-write,-continue}
 sudo emerge dev-util/valgrind --autounmask{,-write,-continue}
+sudo emerge app-text/wgetpaste --autounmask{,-write,-continue}
 
 sudo emerge dev-lang/python --autounmask{,-write,-continue}
 sudo emerge dev-python/pip --autounmask{,-write,-continue}
