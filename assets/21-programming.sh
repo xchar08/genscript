a2e4bb3dd64755f09d20876119873880af9a1a24
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-07-02 03:05:17 +0000

    Update 21-programming.sh

--- assets/21-programming.sh
+++ assets/21-programming.sh
@@ -15,3 +15,6 @@ sudo touch /etc/portage/package.env
 echo "sys-libs/glibc debugsyms installsources" | sudo tee /etc/portage/package.env
 
 sudo mkdir -p /etc/portage/env
+sudo wget https://raw.githubusercontent.com/xchar08/gentoo-secured-dwl/main/assets/debugsyms /etc/portage/env/debugsyms
+sudo wget https://raw.githubusercontent.com/xchar08/gentoo-secured-dwl/main/assets/installsources /etc/portage/env/installsources
+sudo emerge sys-libs/glibc --autounmask{,-write,-continue}
