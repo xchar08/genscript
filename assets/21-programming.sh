ed9781e80211d521d58d7db4ab3fd0594a2a2215
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-07-02 01:41:55 +0000

    Update 21-programming.sh

--- assets/21-programming.sh
+++ assets/21-programming.sh
@@ -8,3 +8,8 @@ sudo emerge app-text/wgetpaste --autounmask{,-write,-continue}
 
 sudo emerge dev-lang/python --autounmask{,-write,-continue}
 sudo emerge dev-python/pip --autounmask{,-write,-continue}
+
+sudo rm -rf /etc/portage/package.env
+sudo touch /etc/portage/package.env
+
+echo "sys-libs/glibc debugsyms installsources" | sudo tee /etc/portage/package.env
