ed2547ddcf833d9692f2d95de8472a164dfa1382
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-07-03 19:03:07 +0000

    Update 2-tar.sh

--- assets/2-tar.sh
+++ assets/2-tar.sh
@@ -4,11 +4,11 @@
 
 echo -n "Enter stage 3 tar url: "
 read -r tarurl
-wget "$tarurl" /mnt/gentoo/"$tarurl"
-tar xvJpf /mnt/gentoo/stage3-*.tar.xz --xattrs --numeric-owner
+wget "$tarurl"
+tar xvJpf stage3-*.tar.xz --xattrs --numeric-owner
 
 # prepare for chroot
 
-mkdir -p /mnt/gentoo/etc/portage/repos.conf
+mkdir /mnt/gentoo/etc/portage/repos.conf
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 cp /etc/resolv.conf /mnt/gentoo/etc/resolv.conf
