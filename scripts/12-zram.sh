4e9ef240b8003ed9456e3a2266a3e94f393517b0
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-08-08 01:11:33 -0500

    Update 12-zram.sh

--- scripts/12-zram.sh
+++ scripts/12-zram.sh
@@ -25,19 +25,18 @@ read -p "Enter the path or device name of the swap partition: " swap_partition
 
 read -p "Do you want to set up regular zram or encrypted zram? (r/e)" choice
 
-case "$choice" in
-  r|R )
-    # Set up regular zram
-    sudo modprobe zram
-    
-    # Unmount and detach zram0 if it's in use
-    unmount_device "/dev/zram0"
-    echo $((6144*1024*1024)) | sudo tee /sys/block/zram0/disksize > /dev/null
-    
-    sudo mkswap /dev/zram0
-    sudo swapon /dev/zram0 -p 10
-    
-    sudo bash -c 'cat << EOF > /etc/local.d/zram.start
+
+# Set up regular zram
+sudo modprobe zram
+
+# Unmount and detach zram0 if it's in use
+unmount_device "/dev/zram0"
+echo $((6144*1024*1024)) | sudo tee /sys/block/zram0/disksize > /dev/null
+
+sudo mkswap /dev/zram0
+sudo swapon /dev/zram0 -p 10
+
+sudo bash -c 'cat << EOF > /etc/local.d/zram.start
 #!/bin/sh
 modprobe zram
 echo $((6144*1024*1024)) > /sys/block/zram0/disksize
@@ -45,41 +44,17 @@ mkswap /dev/zram0
 swapon /dev/zram0 -p 10
 EOF'
 
-    sudo bash -c 'cat << EOF > /etc/local.d/zram.stop
+sudo bash -c 'cat << EOF > /etc/local.d/zram.stop
 #!/bin/sh
 swapoff /dev/zram0
 echo 1 > /sys/block/zram0/reset
 echo 0 > /sys/block/zram0/disksize
 modprobe -r zram
 EOF'
-    
-    sudo chmod +x /etc/local.d/zram.start
-    sudo chmod +x /etc/local.d/zram.stop
-    
-    sudo rc-update add local default
-    ;;
-  e|E )
-    # Generate a random passphrase for encryption
-    passphrase=$(sudo openssl rand -hex 32)
-    
-    # Update /etc/crypttab
-    sudo bash -c "echo 'swap      $swap_partition    /dev/urandom   swap,cipher=aes-xts-plain64,size=8192' >> /etc/crypttab"
-    
-    # Update /etc/fstab
-    sudo bash -c "echo '/dev/mapper/swap  none   swap    defaults   0       0' >> /etc/fstab"
-    
-    # Enable encrypted swap on boot
-    sudo rc-update add swap boot
-    
-    # Set the passphrase for the swap partition
-    sudo bash -c "echo 'swap_crypt UUID=$(sudo blkid -s UUID -o value $swap_partition) none luks,keyscript=decrypt_keyctl' > /etc/conf.d/dmcrypt_swap_passphrase"
-    sudo bash -c "echo 'passphrase=$passphrase' >> /etc/conf.d/dmcrypt_swap_passphrase"
-    
-    # Set the correct permissions for the passphrase file
-    sudo chmod 0600 /etc/conf.d/dmcrypt_swap_passphrase
-    
-    # Update the initramfs
-    sudo dracut --force --regenerate-all
-    ;;
-esac
+
+sudo chmod +x /etc/local.d/zram.start
+sudo chmod +x /etc/local.d/zram.stop
+
+sudo rc-update add local default
+
 echo "Reboot your computer..."
