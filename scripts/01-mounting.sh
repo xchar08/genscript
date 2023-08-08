3a9885d17b22f779addbbab7269c7fda81e22f2d
xchar08 <108827236+xchar08@users.noreply.github.com>
2023-08-08 17:26:47 +0000

    Update 01-mounting.sh

--- scripts/01-mounting.sh
+++ scripts/01-mounting.sh
@@ -1,53 +1,32 @@
 #!/bin/bash
 
-get_partuuid() {
-    blkid | grep "crypto_LUKS" | sed -n 's/.*PARTUUID=\"\([^\"]*\)\".*/\1/p'
-}
+# read in needed volumes
 
-read_partition() {
-    local prompt="$1"
-    local example="$2"
-    local partition
-    read -rp "Enter $prompt partition (e.g. $example): " partition
-    echo "$partition"
-}
-
-create_btrfs_subvolume() {
-    local mount_point="$1"
-    local subvol_name="$2"
-    btrfs subvolume create "$mount_point/$subvol_name"
-}
-
-mount_partition() {
-    local source_device="$1"
-    local target_mount="$2"
-    local subvol_name="$3"
-    mount -o subvol="$subvol_name" "$source_device" "$target_mount"
-}
-
-partuuid=$(get_partuuid)
-
-efipart=$(read_partition "efi" "nvme0n1p1")
-bootpart=$(read_partition "boot" "nvme0n1p2")
+partuuid=$(blkid | grep "crypto_LUKS" | sed -n 's/.*PARTUUID=\"\([^\"]*\)\".*/\1/p')
+echo -n 'Enter efi partition (e.g. nvme0n1p1): '
+read -r efipart
+echo -n 'Enter boot partition (e.g. nvme0n1p2): '
+read -r bootpart
 
 # Create Btrfs filesystem and subvolumes
-mkfs.btrfs "/dev/mapper/luks-$partuuid"
-mount "/dev/mapper/luks-$partuuid" /mnt
+mkfs.btrfs /dev/mapper/luks-"$partuuid"
+mount /dev/mapper/luks-"$partuuid" /mnt
 
 cd /mnt || exit
-
-create_btrfs_subvolume "." "root"
-create_btrfs_subvolume "." "var"
-create_btrfs_subvolume "." "tmp"
-create_btrfs_subvolume "." "home"
+btrfs subvolume create ./root
+btrfs subvolume create ./var
+btrfs subvolume create ./tmp
+btrfs subvolume create ./home
 
 # Mount partitions
 mkdir /mnt/gentoo
-mount_partition "/dev/mapper/luks-$partuuid" "/mnt/gentoo" "root"
-mount_partition "/dev/mapper/luks-$partuuid" "/mnt/gentoo/home" "home"
-mount_partition "/dev/mapper/luks-$partuuid" "/mnt/gentoo/var" "var"
-mount_partition "/dev/mapper/luks-$partuuid" "/mnt/gentoo/tmp" "tmp"
+mount -o subvol=root /dev/mapper/luks-"$partuuid" /mnt/gentoo
 
-mount "/dev/$bootpart" /mnt/gentoo/boot
+mkdir /mnt/gentoo/{boot,home,var,tmp}
+mount -o subvol=home /dev/mapper/luks-"$partuuid" /mnt/gentoo/home
+mount -o subvol=var /dev/mapper/luks-"$partuuid" /mnt/gentoo/var
+mount -o subvol=tmp /dev/mapper/luks-"$partuuid" /mnt/gentoo/tmp
+
+mount /dev/"$bootpart" /mnt/gentoo/boot
 mkdir /mnt/gentoo/boot/efi
-mount "/dev/$efipart" /mnt/gentoo/boot/efi
+mount /dev/"$efipart" /mnt/gentoo/boot/efi
